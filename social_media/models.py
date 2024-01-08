27a6fec8d5f487b6194d50978ebcd8c961d5e312
verbik <verbikyullya@gmail.com>
2024-01-08 14:53:31 +0200

    Add created_at field to post model

--- social_media/models.py
+++ social_media/models.py
@@ -7,8 +7,12 @@ class Hashtag(models.Model):
 
 
 class Post(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
     )
     text_content = models.TextField()
     hashtags = models.ManyToManyField(Hashtag, blank=True)
+
+    class Meta:
+        ordering = ["-created_at"]
