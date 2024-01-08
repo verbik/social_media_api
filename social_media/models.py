d2b5dfc9f90613f51dc744e753b0e69339551718
verbik <verbikyullya@gmail.com>
2024-01-08 15:00:59 +0200

    Added str method to Post and Hashtag

--- social_media/models.py
+++ social_media/models.py
@@ -5,6 +5,9 @@ from django.conf import settings
 class Hashtag(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -16,3 +19,6 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Post by {self.user}. Posted {self.created_at}"
