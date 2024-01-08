b9fdb837557b2af0aa08766f1e8f7824b3ef72fc
verbik <verbikyullya@gmail.com>
2024-01-08 22:56:20 +0200

    - Added possibility to create hashtags when adding posts - Implemented filtering logic by using django-filter - Divided logic into 2 endpoints: my-posts (posts by authorized user) and posts (posts by everyone else but authorized user)

--- social_media/serializers.py
+++ social_media/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from .models import Hashtag, Post
@@ -22,3 +23,36 @@ class PostSerializer(serializers.ModelSerializer):
             "text_content",
             "hashtags",
         )
+
+
+class PostListSerializer(PostSerializer):
+    hashtags = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="name"
+    )
+
+
+class PostHashtagSerializer(serializers.ModelSerializer):
+    """Serializer for hashtag creation when creating a new post instance"""
+
+    hashtags = HashtagSerializer(many=True, read_only=False, allow_empty=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            "id",
+            "created_at",
+            "text_content",
+            "hashtags",
+        )
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            hashtags_data = validated_data.pop("hashtags")
+            post = Post.objects.create(**validated_data)
+            if hashtags_data:
+                hashtags = [
+                    Hashtag.objects.get_or_create(name=tag["name"])[0]
+                    for tag in hashtags_data
+                ]
+                post.hashtags.set(hashtags)
+            return post
--- social_media/views.py
+++ social_media/views.py
@@ -1,19 +1,23 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from .serializers import PostSerializer
+from .filters import PostFilter
+from .serializers import PostSerializer, PostHashtagSerializer, PostListSerializer
 from .models import Post
 
 
-class LikeCommentView:
+class LikeCommentView:  # TODO: complete likes and comments logic
     pass
 
 
 class AllPostsViewSet(viewsets.ModelViewSet, LikeCommentView):
     serializer_class = PostSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PostFilter
 
     def get_queryset(self):
         user = self.request.user
@@ -23,12 +27,29 @@ class AllPostsViewSet(viewsets.ModelViewSet, LikeCommentView):
         return queryset
 
 
-class UserPostsViewSet(viewsets.ModelViewSet, LikeCommentView):
-    serializer_class = PostSerializer
+class UserPostsViewSet(viewsets.ModelViewSet):
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PostFilter
 
     def get_queryset(self):
         user = self.request.user
 
         queryset = Post.objects.filter(user=user)
 
+        hashtag = self.request.query_params.get("#")
+        if hashtag:
+            queryset = queryset.filter(hashtags__name__icontains=hashtag)
+
         return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostListSerializer
+
+        if self.action == "create":
+            return PostHashtagSerializer
+
+        return PostSerializer
+
+    def perform_create(self, serializer) -> None:
+        serializer.save(user=self.request.user)
