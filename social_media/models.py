0e79f3df31cdba1ae8222f62d95367f4a19a2949
verbik <verbikyullya@gmail.com>
2024-01-08 14:50:04 +0200

    Add Post, Hashtag models

--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Hashtag(models.Model):
+    name = models.CharField()
+
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
+    text_content = models.TextField()
+    hashtags = models.ManyToManyField(Hashtag, blank=True)
